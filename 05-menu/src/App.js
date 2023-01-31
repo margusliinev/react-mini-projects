92acb3a7312b02dccc537916a297ef74b472c5f2
Margus Liinev <margus.liinev@gmail.com>
2023-02-01 01:22:46 +0200

    filter menu items, dynamic button

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -3,9 +3,27 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
+const uniqueCategories = items.reduce(
+    (total, current) => {
+        if (!total.includes(current.category)) {
+            total.push(current.category);
+        }
+        return total;
+    },
+    ['all']
+);
+
 function App() {
     const [menuItems, setMenuItems] = useState(items);
-    const [categories, setCategories] = useState([]);
+
+    const filterItems = (category) => {
+        if (category === 'all') {
+            setMenuItems(items);
+            return;
+        }
+        const newItems = items.filter((item) => item.category === category);
+        setMenuItems(newItems);
+    };
 
     return (
         <main>
@@ -14,7 +32,7 @@ function App() {
                     <h2>our menu</h2>
                     <div className='underline'></div>
                 </div>
-                <Categories />
+                <Categories categories={uniqueCategories} filterItems={filterItems} />
                 <Menu items={menuItems} />
             </section>
         </main>
