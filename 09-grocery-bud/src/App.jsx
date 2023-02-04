328e4f78bd73ba4cbb18d6b1f0d75fc2e9f60b40
Margus Liinev <margus.liinev@gmail.com>
2023-02-04 21:19:13 +0200

    remove items from list, alert style fixes

--- 09-grocery-bud/src/App.jsx
+++ 09-grocery-bud/src/App.jsx
@@ -13,18 +13,34 @@ function App() {
     const handleSubmit = (e) => {
         e.preventDefault();
         if (!name) {
+            setAlert({ show: true, msg: 'please enter value', type: 'danger' });
+            setTimeout(() => {
+                setAlert({ show: false, msg: '' });
+            }, 2000);
         } else if (name && isEditing) {
         } else {
+            setAlert({ show: true, msg: 'Item added to the list', type: 'success' });
+            setTimeout(() => {
+                setAlert({ show: false, msg: '' });
+            }, 2000);
             const newItem = { id: new Date().getTime().toString(), title: name };
             setList([...list, newItem]);
             setName('');
         }
     };
 
+    const clearList = () => {
+        setAlert({ show: true, msg: 'items removed from the list', type: 'danger' });
+        setTimeout(() => {
+            setAlert({ show: false, msg: '' });
+        }, 2000);
+        setList([]);
+    };
+
     return (
         <section className='section-center'>
             <form onSubmit={handleSubmit} className='grocery-form'>
-                {alert.show && <Alert />}
+                <Alert {...alert} />
                 <h3>grocery bud</h3>
                 <div className='form-control'>
                     <input type='text' className='grocery' placeholder='e.g eggs' value={name} onChange={(e) => setName(e.target.value)} />
@@ -36,7 +52,9 @@ function App() {
             {list.length > 0 && (
                 <div className='grocery-container'>
                     <List items={list} />
-                    <button className='clear-btn'>clear items</button>
+                    <button className='clear-btn' onClick={clearList}>
+                        clear items
+                    </button>
                 </div>
             )}
         </section>
