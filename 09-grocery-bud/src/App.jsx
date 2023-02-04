857e7e2351da75a0892250c2bfc20bc1f981374f
Margus Liinev <margus.liinev@gmail.com>
2023-02-04 22:29:04 +0200

    adding items to local storage and loading items

--- 09-grocery-bud/src/App.jsx
+++ 09-grocery-bud/src/App.jsx
@@ -3,9 +3,18 @@ import List from './List';
 import Alert from './Alert';
 import './App.css';
 
+const getLocalStorage = () => {
+    let list = localStorage.getItem('list');
+    if (list) {
+        return JSON.parse(localStorage.getItem('list'));
+    } else {
+        return [];
+    }
+};
+
 function App() {
     const [name, setName] = useState('');
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(getLocalStorage);
     const [isEditing, setIsEditing] = useState(false);
     const [editID, setEditID] = useState(null);
     const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
@@ -70,6 +79,13 @@ function App() {
         setEditID(id);
         setName(specificItem.title);
     };
+
+    useEffect(() => {
+        localStorage.setItem('list', JSON.stringify(list));
+        if (list.length < 1) {
+            localStorage.removeItem('list');
+        }
+    }, [list]);
 
     return (
         <section className='section-center'>
