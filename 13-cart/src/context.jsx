9383f5e66db4c4fbab17279219d055328b68824c
Margus Liinev <margus.liinev@gmail.com>
2023-02-07 23:24:13 +0200

    increase, decrease items in cart

--- 13-cart/src/context.jsx
+++ 13-cart/src/context.jsx
@@ -20,6 +20,12 @@ const AppProvider = ({ children }) => {
     const remove = (id) => {
         dispatch({ type: 'REMOVE', payload: id });
     };
+    const increase = (id) => {
+        dispatch({ type: 'INCREASE', payload: id });
+    };
+    const decrease = (id) => {
+        dispatch({ type: 'DECREASE', payload: id });
+    };
 
     return (
         <AppContext.Provider
@@ -27,6 +33,8 @@ const AppProvider = ({ children }) => {
                 ...state,
                 clearCart,
                 remove,
+                increase,
+                decrease,
             }}
         >
             {children}
