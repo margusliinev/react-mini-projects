5e2c8e292fb9cfb58260d390db54c38747d89c4b
Margus Liinev <margus.liinev@gmail.com>
2023-02-14 21:59:49 +0200

    fetch and display single drink data on new page

--- 20-cocktails/src/pages/SingleCocktail.jsx
+++ 20-cocktails/src/pages/SingleCocktail.jsx
@@ -1,5 +1,87 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Loading from '../components/Loading';
+import { useParams, Link } from 'react-router-dom';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
 const SingleCocktail = () => {
-    return <section>SingleCocktail</section>;
+    const { id } = useParams();
+    const [loading, setLoading] = useState(false);
+    const [cocktail, setCocktail] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        const getCocktail = async () => {
+            try {
+                const response = await axios(`${url}${id}`);
+                const data = response.data.drinks[0];
+                if (data) {
+                    const { strDrink: name, strDrinkThumb: image, strAlcoholic: info, strCategory: category, strGlass: glass, strInstructions: instructions, strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5 } = data;
+                    const ingredients = [strIngredient1, strIngredient2, strIngredient3, strIngredient4, strIngredient5];
+                    const newCocktail = { name, image, info, category, glass, instructions, ingredients };
+                    setCocktail(newCocktail);
+                } else {
+                    setCocktail(null);
+                }
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setLoading(false);
+            }
+        };
+        getCocktail();
+    }, [id]);
+
+    if (loading) {
+        return <Loading />;
+    }
+
+    if (!cocktail) {
+        <section>
+            <h2 className='fetch-error'>no cocktail to display</h2>
+        </section>;
+    } else {
+        const { name, image, info, category, glass, instructions, ingredients } = cocktail;
+        return (
+            <section className='cocktail-section'>
+                <Link to='/' className='go-home-btn'>
+                    Back Home
+                </Link>
+                <h2 className='cocktail-title'>{name}</h2>
+                <div className='drink'>
+                    <img src={image} alt={name} />
+                    <div className='drink-info'>
+                        <p>
+                            <span className='drink-data'>name: </span>
+                            {name}
+                        </p>
+                        <p>
+                            <span className='drink-data'>category: </span>
+                            {category}
+                        </p>
+                        <p>
+                            <span className='drink-data'>info: </span>
+                            {info}
+                        </p>
+                        <p>
+                            <span className='drink-data'>glass: </span>
+                            {glass}
+                        </p>
+                        <p>
+                            <span className='drink-data'>instructions: </span>
+                            {instructions}
+                        </p>
+                        <p>
+                            <span className='drink-data'>ingredients:</span>
+                            {ingredients.map((item, index) => {
+                                return item ? <span key={index}>{item}</span> : null;
+                            })}
+                        </p>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 };
 
 export default SingleCocktail;
